8ebdb3fa4211296872cd0fb878d670204290a08e
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-22 14:47:52 +0500

    Add UI to the game with styling.
    Added a GUI to the game, so that users can play without the console.
    Also added styling to the GUI.

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // Constants
-const ROUNDS = 5; // Stores the number of rounds to be played
+let playerScore = 0 // Stores the player's score
+let computerScore = 0 // Stores the computer's score
 
-let playerScore = 0; // Stores the player's score
-let computerScore = 0; // Stores the computer's score
+const rockButton = document.querySelector("#rock-btn")
+const paperButton = document.querySelector("#paper-btn")
+const scissorsButton = document.querySelector("#scissors-btn")
+const restartButton = document.querySelector("#restart")
+
+const modal = document.querySelector("#restart-modal")
+
+const results = document.querySelector("#results")
+const winnerText = document.querySelector("#winner")
+
+const playerScoreText = document.querySelector("#player-score")
+const computerScoreText = document.querySelector("#computer-score")
+
+rockButton.addEventListener("click", () => handleClick("Rock"))
+paperButton.addEventListener("click", () => handleClick("Paper"))
+scissorsButton.addEventListener("click", () => handleClick("Scissors"))
+
+restartButton.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    modal.style.display = "none"
+    updateScore()
+})
+
+function handleClick(playerSelection) {
+    playRound(playerSelection, getComputerChoice())
+}
 
 // Get the computer's choice of move to play
 function getComputerChoice() {
@@ -12,83 +38,69 @@ function getComputerChoice() {
 
 // Plays a single round of Rock, Paper, Scissors
 function playRound(playerSelection, computerSelection) {
-    let won = false // Stores if the player has won
+    let winner = "" // Stores the winner of the round
 
     // Lower Case versions of the selections, to make the conditions easier
     let lowerPlayerSelection = playerSelection.toLowerCase()
     let lowerComputerSelection = computerSelection.toLowerCase()
     
-    // If the game is a tie, return null
+    // If the game is a tie, set the winner to tie
     if (lowerPlayerSelection === lowerComputerSelection) {
-        return null
+        winner = "tie"
     // If the player has won, set won to true, and increase the score
     } else if (
         (lowerPlayerSelection  === "rock" && lowerComputerSelection === "scissors") ||
         (lowerPlayerSelection === "paper" && lowerComputerSelection === "rock") ||
         (lowerPlayerSelection === "scissors" && lowerComputerSelection === "paper")
     ) {
-        won = true
+        winner = "player"
         playerScore++
     // Else if the player has lost, set won to false
     } else {
-        won = false
+        winner = "computer"
         computerScore++
     }
 
-    // Return a win or loss message
-    return (won == true) ? `You Won! ${playerSelection} beats ${computerSelection}` : `You Lose! ${computerSelection} beats ${playerSelection}`
+    displayWinner(winner, playerSelection, computerSelection)
+    updateScore()
 }
 
-// Plays a whole game of Rock, Paper, Scissors, depending on the ROUNDS constant
-function game() {
-    // Play ROUNDS number of rounds
-    for (let i = 0; i < ROUNDS; i++) {
-        let result; // Result string of the current game
-
-        // Keep playing until it isn't a tie
-        do {
-            let playerSelection; // Stores the move selection of the player
-
-            // Keep prompting the user for a move until it is a valid move
-            do {
-                // If the user cancels the prompt, then reprompt
-                do {
-                    playerSelection = prompt("What do you want to choose: 'Rock', 'Paper', or 'Scissors.'")    
-                } while (playerSelection === null);
-            } while (
-                playerSelection.toLowerCase() !== "rock" && 
-                playerSelection.toLowerCase() !== "paper" &&
-                playerSelection.toLowerCase() !== "scissors"
-            );
-            
-            // Plays a round of Rock, Paper, Scissors
-            result = playRound(playerSelection, getComputerChoice())
-            
-            // If the game is a tie, Display a message, and continue with the next iteration
-            if (result === null) {
-                console.log("It's a TIE! Replay the round.")
-                continue
-            }
-            
-            // Displays the result and the score
-            console.log(`${result}`)
-            console.log(`Your score is ${playerScore} while the computer's score is ${computerScore}.`)
-        } while (result === null);
-    }
-
-    // Displays the winner and the score
-    if (playerScore > computerScore) {
-        console.log("YOU WON THE GAME!")
-        console.log(`You had ${playerScore} points, while the computer had ${computerScore}.`)
+function displayWinner(winner, playerSelection, computerSelection) {
+    if (playerScore === 5) {
+        results.textContent = "You WON the game!"
+        winnerText.textContent = "You WON the game!"
+        modal.style.display = "block"
+    } else if (computerScore === 5) {
+        results.textContent = "You LOST the game!"
+        winnerText.textContent = "You LOST the game!"
+        modal.style.display = "block"
     } else {
-        console.log("You lost the game!")
-        console.log(`You had ${playerScore} points, while the computer had ${computerScore}.`)
+        switch (winner.toLowerCase()) {
+            case "player":
+                results.textContent = `You Won! ${playerSelection} beats ${computerSelection}`
+                break;
+            
+            case "computer":
+                results.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`
+                break;
+            
+            case "tie":
+                results.textContent = `It's a TIE! Both player chose ${playerSelection}`
+                break;
+
+            default:
+                results.textContent = "ERROR"
+                break;
+        }
     }
+}
+
+function updateScore() {
+    playerScoreText.textContent = `Your Score: ${playerScore}`
+    computerScoreText.textContent = `Computer's score: ${computerScore}`
 }
 
 // Returns a random item from a list
 function randomChoice(items) {
     return items[Math.floor(Math.random() * items.length)]
 }
-
-game(); // Plays the game
